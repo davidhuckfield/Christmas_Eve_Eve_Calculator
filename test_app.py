c031e818ffc5eb2c95a94fd0f5d2f64f0a86a507
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-10-13 14:28:48 +0100

    Initial setup and logic

--- test_app.py
+++ test_app.py
@@ -1,12 +1,4 @@
-from flask import Flask, render_template
 from datetime import datetime, date
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    message = xmas_eve_calc()
-    return render_template('index.html', message=message)
 
 def xmas_eve_calc():
     input_date = date(2024, 10, 13)
@@ -22,7 +14,5 @@ def xmas_eve_calc():
     return_string += str(days_number)
     return_string += " days until Christmas!"
     print(return_string)
-    return return_string
 
-if __name__ == '__main__':
-    app.run(debug=True)
+xmas_eve_calc()
