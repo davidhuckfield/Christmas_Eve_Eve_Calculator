987146af05c2f77d91e77038972cafb244b974b6
davidhuckfield <djhuckfield@hotmail.co.uk>
2025-01-04 18:47:35 +0000

    testing hosting site

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from datetime import datetime, date
 
+#copy from here for PE hosting
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
